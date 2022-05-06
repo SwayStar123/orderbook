60590902c54b5c6980f73f3ea6d4bebf3c2c3837
SwayStar123 <swayambhanded@gmail.com>
2022-05-06 17:59:08 +0530

    order matching

--- src/orderbook.rs
+++ src/orderbook.rs
@@ -9,6 +9,7 @@ pub struct User {
     pub orders: Vec<Order>,
 }
 
+#[derive(Clone)]
 pub struct Order {
     pub user: &'static User,
     pub id: u64,
@@ -39,9 +40,9 @@ impl OrderBook {
         match price_level {
             Some(price_level) => {
                 if order.quantity > 0 {
-                    price_level.bids.push(order);
+                    price_level.bids.push(order.clone());
                 } else {
-                    price_level.asks.push(order);
+                    price_level.asks.push(order.clone());
                 }
             }
             None => {
@@ -52,12 +53,31 @@ impl OrderBook {
                 });
             }
         }
+        
+        self.match_orders(order.price);
     }
 
     pub fn match_orders(&mut self, price: u64) {
         let price_level = self.prices.iter_mut().find(|price_level| price_level.price == price);
 
-        
+        //if any bids and asks coexist at this price, match them
+        if let Some(price_level) = price_level {
+            while !price_level.bids.is_empty() && !price_level.asks.is_empty() {
+                //whichever is smaller, take that quantity
+                let quantity = std::cmp::min(price_level.bids[0].quantity, price_level.asks[0].quantity);
+
+                //remove the smaller order
+                if price_level.bids[0].quantity < price_level.asks[0].quantity {
+                    price_level.bids.pop();
+                    price_level.asks[0].quantity -= quantity;
+
+                } else {
+                    price_level.asks.pop();
+                    price_level.bids[0].quantity -= quantity;
+                }
+
+            }
+        }
     }
 
 }
